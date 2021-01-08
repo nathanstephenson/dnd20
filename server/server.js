449eb1147df8ea6dfc0a849789d76c93419bc634
nathanstephenson <nathans261@gmail.com>
2021-01-08 20:12:07 +0000

    .

--- server/server.js
+++ server/server.js
@@ -3,10 +3,21 @@ const path = require(`path`);
 const app = express();
 const port = process.env.PORT || 8080;
 const publicPath = path.join(__dirname, '..', 'client');
+const { ApolloServer } = require('apollo-server-express');
+//const { typeDefs, resolvers } = require('./schema');
+const server = new ApolloServer({
+    //typeDefs,
+    //resolvers,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.static(publicPath));
 
 app.use(express.static(path.join(publicPath, 'build')));
+
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
