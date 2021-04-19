babc6eff4e3a0c3254acd49e243f218fa12b341f
nathanstephenson <nathans261@gmail.com>
2021-04-19 14:28:04 +0100

    added subscription link to apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,33 @@ import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+import {ApolloClient, ApolloProvider, InMemoryCache, split, HttpLink} from '@apollo/client';
+import {getMainDefinition} from '@apollo/client/utilities'
+import {WebSocketLink} from '@apollo/client/link/ws'
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:8080/subscriptions',
+  options: {reconnect:true}
+})
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const splitLink = split(({query})=>{
+  const definition = getMainDefinition(query)
+  return (
+    definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+  )
+}, wsLink, httpLink)
+
 /*
 const PORT = ':8080';
 const HOST = '192.169.0.49';*/
 //create apollo client
 export const apolloClient = new ApolloClient({
   uri: '/graphql',//this is only the extension to the current url to send gql requests to
+  link: splitLink,
   cache: new InMemoryCache(),
   onError: ({ networkError, graphQLErrors }) => {
     console.log('graphQLErrors', graphQLErrors)
