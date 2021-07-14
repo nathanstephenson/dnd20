b4ca3d4157c0de5d5247cc7570d8e78080c208ce
nathanstephenson <nathans261@gmail.com>
2021-07-14 21:51:50 +0100

    added user info

--- client/src/pages/MyAccount.js
+++ client/src/pages/MyAccount.js
@@ -6,10 +6,12 @@ import {getUserID} from '../queries'
 
 export default function MyAccount(props) {
     const {user} = useContext(UserContext)
-
     return (
         <>
-
+            <div>Name: {user.name}</div>
+            <div>Username: {user.username}</div>
+            <div>Campaigns participated in: {user.campaigns.length} (DM of {user.campaigns.filter(current => current.dm===user._id).length})</div>
+            <div>Characters: {user.characters.length}</div>
         </>
     )
 }
