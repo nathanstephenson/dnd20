633c41dd246c7138b45242db010f228ffdd0d5e8
nathanstephenson <nathans261@gmail.com>
2021-08-07 02:21:51 +0100

    map now works for first user in each square, and updates when someone moves

--- client/src/components/play/map.js
+++ client/src/components/play/map.js
@@ -10,9 +10,45 @@ export function Map(props){
     const session = props.session
     const w = 20
     const h = 15
-    const pWidth = 30
+    const pWidth = 35
     const [rows, setRows] = useState()
     const [changePos, {data, loading}] = useMutation(changeCharacterPos)
+    
+    let grid = []
+    useEffect(()=>{
+        function select(e){
+            const n = Number.parseInt(e.target.attributes.name.value)
+            changePos({variables: {session:session._id, character:props.character, position:n}})
+        }
+        for(let row=0;row<h;row++){
+            let line = []
+            for(let column=0;column<w;column++){//each square in the grid
+                let image = ""
+                const i = row*w + column
+                const here = props.session.characters.filter(el => el.position===i)
+                if(here.length>0){
+                    console.log(here, here[0].position)
+                    image = "images/Nooth_DnD.png"
+                }else{
+                    image = ""
+                }
+                const occupied = here.length>0
+                line.push(
+                    <div width={pWidth} height={pWidth} occupied={occupied} className="square">
+                        <svg width={pWidth} height={pWidth} className="grid-square"><g>
+                            <rect width={pWidth} height={pWidth} id="rec" name={"bg"} className="grid-square-bg"/>
+                            <text width={pWidth} height={pWidth}>W</text> 
+                            <image href={image} width={pWidth-1} height={pWidth-1}/>
+                            <rect width={pWidth} height={pWidth} id="rec" name={i} className="grid-square-fg" onClick={select}/>
+                        <p>W</p></g></svg>
+                    </div>
+                )
+            } 
+            grid.push(<div width={w*pWidth} height={pWidth} className="row">{line}</div>)
+        }
+        setRows(grid)
+    },[session])
+    
     /* useEffect(()=>{
         const grid = document.getElementById("grid")
         if(window.innerWidth>=window.innerHeight){
@@ -20,36 +56,10 @@ export function Map(props){
         }else{
             grid.setAttribute("transform", "rotate(45)")
         }
-        //grid.appendChild(rows)
         console.log(grid)
-    },[rows]) */
-    useEffect(()=>{
-        var i = 1
-        let grid = []
-        for(let row=1;row<=h;row++){
-            let row = []
-            for(let column=1;column<=w;column++){//each square in the grid
-                const occupied = i===4
-                row.push(
-                    <div width={pWidth} height={pWidth} className="square">
-                        <svg width={pWidth} height={pWidth} className="grid-square"><g>
-                            <rect width={pWidth} height={pWidth} id="rec" name={"bg"} className="grid-square-bg"/>
-                            <text width={pWidth} height={pWidth}>W</text> 
-                            {occupied && <image href="images/Nooth_DnD.png" width={pWidth-1} height={pWidth-1}/>}
-                            <rect width={pWidth} height={pWidth} id="rec" name={i} innerHTML={column} className="grid-square-fg" onClick={select}/>
-                        <p>W</p></g></svg>
-                    </div>
-                )
-                i = i+1
-            } 
-            grid.push(<div width={w*pWidth} height={pWidth} className="row">{row}</div>)
-        }
-        setRows(grid)
-    },[])
-    function select(e){
-        const n = Number.parseInt(e.target.attributes.name.value)
-        console.log(n)
-    }
+    },[]) */
+    
+
     return (
         <>
             <div width={w*pWidth} height={h*pWidth} id="grid" className="grid">{rows}</div>
